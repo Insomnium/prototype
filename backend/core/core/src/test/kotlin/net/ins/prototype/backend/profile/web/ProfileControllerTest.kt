bdbbdd888b53407a89415b9e0a2e5c462b0c891e
Insomnium <ins137@gmail.com>
2025-04-27 20:18:35 +0400

    Cover profiles API with basic integration test

--- backend/core/core/src/test/kotlin/net/ins/prototype/backend/profile/web/ProfileControllerTest.kt
+++ backend/core/core/src/test/kotlin/net/ins/prototype/backend/profile/web/ProfileControllerTest.kt
@@ -6,22 +6,25 @@ import com.github.springtestdbunit.annotation.DatabaseTearDown
 import net.ins.prototype.backend.conf.TestcontainersConfiguration
 import net.ins.prototype.backend.meta.TestProfile
 import net.ins.prototype.backend.profile.dao.repo.ProfileRepository
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType
 import org.springframework.test.context.TestExecutionListeners
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
 @Import(TestcontainersConfiguration::class)
 @TestExecutionListeners(
     DependencyInjectionTestExecutionListener::class,
     DbUnitTestExecutionListener::class,
 )
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @TestProfile
 @DatabaseTearDown("classpath:/dbunit/0001/profiles-cleanup.xml")
@@ -37,6 +40,29 @@ class ProfileControllerTest {
     @DisplayName("Should respond with found profiles")
     @DatabaseSetup("classpath:/dbunit/0001/profiles.xml")
     fun shouldFindProfiles() {
-        val debug = "debug"
+        mockMvc.get("/v1/profiles") {
+            accept = MediaType.APPLICATION_JSON
+            queryParam("gender", "FEMALE")
+            queryParam("purposes", "DATING,SEXTING")
+        }.andExpect {
+            status { isOk() }
+            content {
+                contentType(MediaType.APPLICATION_JSON)
+                jsonPath("$.profiles") { isArray() }
+                jsonPath("$.profiles.length()") { value(2) }
+
+                jsonPath("$.profiles[0].id") { value("1") }
+                jsonPath("$.profiles[0].gender") { value("FEMALE") }
+                jsonPath("$.profiles[0].birth") { value("1996-11-17") }
+                jsonPath("$.profiles[0].purposes.length()") { value(2) }
+                jsonPath("$.profiles[0].purposes") { containsInAnyOrder("DATING", "RELATIONSHIPS") }
+
+                jsonPath("$.profiles[1].id") { value("2") }
+                jsonPath("$.profiles[1].gender") { value("FEMALE") }
+                jsonPath("$.profiles[1].birth") { value("1996-12-17") }
+                jsonPath("$.profiles[1].purposes.length()") { value(2) }
+                jsonPath("$.profiles[1].purposes") { containsInAnyOrder("DATING", "SEXTING") }
+            }
+        }
     }
 }
