891bc0b9878f26b998250f7a85a0f0bcf123fe33
Insomnium <ins137@gmail.com>
2025-06-03 21:06:17 +0400

    Add images upload API

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+FS_IMAGES_FOLDER="./photo"
+
 echo "Cleaning up profiles from postgres"
+docker exec postgres-prototype psql -U postgres -c "delete from images"
 docker exec postgres-prototype psql -U postgres -c "delete from profiles"
 
 echo "Removing ES profile index"
@@ -8,5 +11,8 @@ curl -XDELETE http://localhost:9200/profile
 
 echo "Removing Kafka profile topic"
 docker exec broker bash -c "kafka-topics --bootstrap-server localhost:9092 --topic profile --delete"
+
+echo "Removing images"
+find $FS_IMAGES_FOLDER -mindepth 1 -maxdepth 2 -type f ! -name '.gitkeep' -exec rm -f {} +
 
 echo "Done"
