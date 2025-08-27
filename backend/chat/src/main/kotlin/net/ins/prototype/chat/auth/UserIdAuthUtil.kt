371e50bb25040479cf744e612b0b3e248328a48f
Insomnium <ins137@gmail.com>
2025-08-27 18:41:37 +0400

    [chat] unify utility extension functions

--- backend/chat/src/main/kotlin/net/ins/prototype/chat/auth/UserIdAuthUtil.kt
+++ backend/chat/src/main/kotlin/net/ins/prototype/chat/auth/UserIdAuthUtil.kt
@@ -1,19 +1,22 @@
 package net.ins.prototype.chat.auth
 
+import net.ins.prototype.chat.auth.exception.MissingMandatoryHeaderException
+import net.ins.prototype.chat.auth.exception.MissingReceiverHeaderException
 import net.ins.prototype.chat.auth.exception.MissingSenderHeaderException
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.NativeMessageHeaderAccessor
 
 object P2pHeaders {
     const val SENDER = "X-sender-id"
     const val RECEIVER = "X-receiver-id"
 }
 
-val StompHeaderAccessor.senderId: String
-    get() = getNativeHeader(P2pHeaders.SENDER)?.first() ?: throw MissingSenderHeaderException()
+val <T : NativeMessageHeaderAccessor> T.senderId: String
+    get() = getNativeHeader(P2pHeaders.SENDER) { MissingSenderHeaderException() }
 
-val SimpMessageHeaderAccessor.receiverId: String
-    get() = getNativeHeader(P2pHeaders.RECEIVER)?.first() ?: throw MissingSenderHeaderException()
+val <T : NativeMessageHeaderAccessor> T.receiverId: String
+    get() = getNativeHeader(P2pHeaders.RECEIVER) { MissingReceiverHeaderException() }
 
-val SimpMessageHeaderAccessor.senderId: String
-    get() = getNativeHeader(P2pHeaders.SENDER)?.first() ?: throw MissingSenderHeaderException()
+private fun NativeMessageHeaderAccessor.getNativeHeader(
+    key: String,
+    onException: () -> MissingMandatoryHeaderException,
+): String = getNativeHeader(key)?.first() ?: throw onException()
