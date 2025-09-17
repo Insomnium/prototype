a6f32519b8927957e20b72dbf51469d291db1f47
Insomnium <ins137@gmail.com>
2025-09-17 15:10:40 +0400

    fix build

--- backend/chat/src/main/kotlin/net/ins/prototype/chat/dao/P2pMessageEntity.kt
+++ backend/chat/src/main/kotlin/net/ins/prototype/chat/dao/P2pMessageEntity.kt
@@ -2,12 +2,10 @@ package net.ins.prototype.chat.dao
 
 import java.io.Serializable
 
-@
 data class P2pMessageEntity(
     val messageId: String,
 )
 
-@
 data class P2pMessagePk(
     val roomId: String,
     val messageId: String,
