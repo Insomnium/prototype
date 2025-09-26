6c940742fcdc2f945f1219c64756ec1767900bb3
Insomnium <ins137@gmail.com>
2025-09-26 15:39:27 +0400

    [FE] refactor chat window, handle textarea state

--- frontend/src/component/ChatWindow.jsx
+++ frontend/src/component/ChatWindow.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { getSelectedContact } from "../store/contactListSlice";
+import { useState } from "react";
 
 const ChatWindow = () => {
 
@@ -9,21 +10,51 @@ const ChatWindow = () => {
         <>
             <div className="chat-window">
                 <ChatWindowHeader selectedContact={selectedContact} />
-
-                <div className="chat-input-container">
-                    <textarea className="chat-input" id="message-input" placeholder="Type a message..." rows="1"></textarea>
-                    <button className="send-button" id="send-button" disabled>
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M22 2L11 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M22 2L15 22L11 13L2 9L22 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                  stroke-linejoin="round"/>
-                        </svg>
-                    </button>
-                </div>
+                <ChatInput />
             </div>
         </>
     )
-}
+};
+
+const ChatInput = () => {
+
+    const [ input, setInput ] = useState('')
+    const [ submitionDisabled, setSubmitionDisabled ] = useState(true)
+
+    const onChange = (event) => {
+        const value = event.target.value
+        setSubmitionDisabled(value == null || value == '')
+        setInput(value)
+    }
+
+    const handleSubmit = () => {
+        console.log(`>>> ${input}`)
+        setSubmitionDisabled(true)
+        setInput('')
+    }
+
+    const interceptKeyDown = (event) => {
+        if (event.ctrlKey && event.key == 'Enter') {
+            event.preventDefault();
+            handleSubmit();
+        }
+    }
+
+    return (
+        <>
+        <div className="chat-input-container">
+            <textarea id="message-input" className="chat-input" placeholder="Type a message..." rows="1" 
+                value={input} 
+                onChange={onChange}
+                onKeyDown={interceptKeyDown}
+                />
+            <button className="send-button" id="send-button" disabled={submitionDisabled} onClick={handleSubmit}>
+                <ChatSubmitButtonSvg />
+            </button>
+        </div>
+        </>
+    )
+};
 
 const ChatWindowHeader = ({ selectedContact }) => {
     return (
@@ -51,7 +82,7 @@ const ChatHeaderStatus = ({status}) => {
         return (
             <div className="status-container" >
                 <p id="current-contact-status">{status}</p>
-                <span id="status-indicator" class="status-indicator-online" />
+                <span id="status-indicator" className="status-indicator-online" />
             </div>
         )
     }
@@ -74,6 +105,18 @@ const ChatHeaderContactPlate = ({ selectedContact }) => {
     }
     return (
         <h3 id="current-contact-name">Select a contact to start chatting</h3>
+    )
+};
+
+const ChatSubmitButtonSvg = () => {
+    return (
+        <>
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22 2L11 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M22 2L15 22L11 13L2 9L22 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round"/>
+            </svg>
+        </>
     )
 }
 
